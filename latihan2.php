6830794ac69dbb8fb1cc82a555762373937da018
korius kussa wenda <koriuswenda@gmail.com>
2023-09-24 17:13:42 +0700

    hasil kerja tugas SaCode-php-pertemuan-5

---
+++ latihan2.php
@@ -0,0 +1,21 @@
+<?php
+// pengkondisian / percabangan
+// if else
+// if else if else
+// ternary
+// switch
+
+$x = 2;
+if ($x < 20 ) {
+    echo "benar";
+}  else if ($x == 20 )
+        echo "bingo!";
+
+else {
+    echo "salah";
+}
+
+
+
+
+?>
